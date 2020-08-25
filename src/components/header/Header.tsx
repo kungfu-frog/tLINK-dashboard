dae7b78122e9f9bca43a6f29190f5f296f13a21a
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-08-25 17:44:16 -0300

    logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { Container } from 'components';
+import LogoImage from 'assets/img/logo.png';
 
 interface Props {}
 
@@ -8,7 +9,10 @@ const Header: React.FC<Props> = () => {
   return (
     <Container>
       <div className="nav-header">
-        <div>LinkStepper</div>
+        <div className="center-v">
+          <img className="logo-image" src={LogoImage} alt='LinkStepper' />
+          <span className="logo-text">LinkStepper</span>
+        </div>
         <Button variant='contained' className='btn-header'>Connect Wallet</Button>
       </div>
     </Container>
